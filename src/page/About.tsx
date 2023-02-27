711cf31e51af0c3c30db3e3d2a7010c7488bbc93
CHOGANGYEOL <loeygnak@gmail.com>
2023-02-27 22:50:55 +0900

    feat:about page markup

--- src/page/About.tsx
+++ src/page/About.tsx
@@ -1,7 +1,55 @@
+import styled from "styled-components";
+import profileImg from "../image/profileImg.jpg";
+
+const AboutWrap = styled.article`
+  display: flex;
+  flex-direction: column;
+  min-height: calc(100vh - 8rem);
+  justify-content: center;
+  align-items: center;
+  .flexWrap {
+    display: flex;
+    width: 100%;
+    .imgWrap {
+      flex-basis: 40%;
+      border-radius: 10px;
+      overflow: hidden;
+      > img {
+        max-width: 100%;
+      }
+    }
+    .desc {
+      flex-basis: 60%;
+      padding-left: 10rem;
+    }
+  }
+  .desc {
+    color: var(--gray-color);
+    margin-bottom: 2rem;
+  }
+`;
 
 const About = () => {
-    return(
-        <p>this is about</p>
-    )
-}
-export default About
+  return (
+    <AboutWrap>
+      <div className="flexWrap">
+        <div className="imgWrap">
+          <img src={profileImg} alt="profileImg" />
+        </div>
+        <div className="desc">
+          <h2>KANGYEOL CHO</h2>
+          <p>
+            <span>Frontend</span> Developer
+          </p>
+          <p>
+            Hello, I'm KANGYEOL CHO and happy developer. I am currently working
+            on Front-End (React) development. This is a space that introduces me
+            and records my growth log. This is like my growing up diary. Please
+            feel free to contact me if you have any questions. Thank you!
+          </p>
+        </div>
+      </div>
+    </AboutWrap>
+  );
+};
+export default About;
