b1bd301f675dab12506a680027ca61acf2c0778b
CHOGANGYEOL <rkdduf06@icloud.com>
2023-08-29 14:29:26 +0900

    fix:data

--- src/data/PortfolioData.js
+++ src/data/PortfolioData.js
@@ -6,13 +6,13 @@ export const PortfolioData = [
     link: "",
   },
   {
-    title: "Fulldive",
-    description: "실시간 스트리밍 플랫폼 프론트엔드개발",
+    title: "Fulldive Admin",
+    description: "Fulldive 관리자페이지 프론트엔드개발",
     image: "",
     link: "",
   },
   {
-    title: "Fulldive",
+    title: "RAYWING",
     description: "실시간 스트리밍 플랫폼 프론트엔드개발",
     image: "",
     link: "",
