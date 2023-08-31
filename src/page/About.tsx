5099dbd526d4579041767fc733c34d2f88ac6e85
CHOGANGYEOL <rkdduf06@icloud.com>
2023-08-31 14:20:08 +0900

    feat:stack

--- src/page/About.tsx
+++ src/page/About.tsx
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
+const language = ["JavaScript", "TypeScript", "HTML5", "CSS3"];
+
+const library = [
+  "React",
+  "Axios",
+  "React-query",
+  "Redux",
+  "Redux persist",
+  "Stomp.js",
+  "i18next",
+  "Styled Component",
+  "jQuery",
+];
+
+const protocol = ["HTTP", "webSocket", "REST API"];
+
+const etc = ["Git", "Photoshop", "XD", "Figma", "Illustrator", "Notion", "Trello"];
+
 const About = () => {
   return (
     <AboutWrap>
@@ -9,6 +27,40 @@ const About = () => {
         </div>
         <p className="static">Developer</p>
         <p>CHO GANGYEOL</p>
+      </div>
+      <div className="body">
+        <div>
+          <h4>Language</h4>
+          <ul className="">
+            {language.map((el, idx) => {
+              return <li key={idx}>{el}</li>;
+            })}
+          </ul>
+        </div>
+        <div>
+          <h4>Library</h4>
+          <ul className="">
+            {library.map((el, idx) => {
+              return <li key={idx}>{el}</li>;
+            })}
+          </ul>
+        </div>
+        <div>
+          <h4>Protocol</h4>
+          <ul className="">
+            {protocol.map((el, idx) => {
+              return <li key={idx}>{el}</li>;
+            })}
+          </ul>
+        </div>
+        <div>
+          <h4>etc.</h4>
+          <ul className="">
+            {etc.map((el, idx) => {
+              return <li key={idx}>{el}</li>;
+            })}
+          </ul>
+        </div>
       </div>
     </AboutWrap>
   );
@@ -43,6 +95,32 @@ const AboutWrap = styled.section`
     }
   }
 
+  .body {
+    margin-top: 36px;
+    max-width: 80rem;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    h4 {
+      font-size: var(--big-font);
+      /* color:#ccc; */
+    }
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      flex-direction: row;
+      gap: 6px;
+      margin-top: 6px;
+      li {
+        padding: 2px 8px;
+        color: #000;
+        background-color: #ccc;
+        border-radius: 100px;
+        font-size: var(--small-font);
+        font-weight: 500;
+      }
+    }
+  }
   @keyframes swapthings {
     0% {
       font-variation-settings: "wght" 275, "wdth" 50;
