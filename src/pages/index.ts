a8dad73b213073386f95c5321c7912e742eff646
CHOGANGYEOL <rkdduf06@icloud.com>
2024-05-20 08:57:31 +0900

    :memo: Router add

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,21 @@
+import Blog from "./Blog";
 import Games from "./Games";
 import Canvas from "./Games/Canvas";
 import OMok from "./Games/OMok";
 import Main from "./Main";
 import NotFound from "./NotFound";
+import Profile from "./Profile";
+import Project from "./Project";
+import Terminal from "./Terminal";
 
-export const Pages = { Main, Games, OMok, Canvas, NotFound };
+export const Pages = {
+  Main,
+  Games,
+  OMok,
+  Canvas,
+  NotFound,
+  Blog,
+  Profile,
+  Project,
+  Terminal,
+};
